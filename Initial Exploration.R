63384ca5b835f86b1012819a7c8a000b1f9ecd20
BuildTools <unconfigured@null.spigotmc.org>
2017-03-18 14:19:43 -0700

    changed to gym_df

--- Initial Exploration.R
+++ Initial Exploration.R
@@ -5,10 +5,10 @@
 rm(list=ls())
 
 # Set working directory
-setwd("~/Data Science Projects/gymtraffic")
+setwd("~/Data Science Projects/GymTraffic")
 
 # Load csv
-GymTraffic <- read.csv("Data/data.csv")
+gym_df <- read.csv("Data/data.csv")
 
 # Load libraries
 library(ggplot2)
@@ -19,32 +19,32 @@ library(ggplot2)
 
 
 # Number of people
-summary(GymTraffic$number_people)
-p1 <- ggplot(GymTraffic, aes(number_people))
+summary(gym_df$number_people)
+p1 <- ggplot(gym_df, aes(number_people))
 p1 +
   geom_histogram(binwidth = 1) +
   labs(title = "Gym Traffic Distribution", x = "Number of People", y = "Count")
 
 
 # Timestamp
-summary(GymTraffic$timestamp)
-p2 <- ggplot(GymTraffic, aes(timestamp))
+summary(gym_df$timestamp)
+p2 <- ggplot(gym_df, aes(timestamp))
 p2 +
   geom_histogram(binwidth = 600) +
   labs(title = "Timestamps Distribution", x = "Timestamp", y = "Count")
 
 
 # Day of week
-summary(GymTraffic$day_of_week)
-p3 <- ggplot(GymTraffic, aes(day_of_week))
+summary(gym_df$day_of_week)
+p3 <- ggplot(gym_df, aes(day_of_week))
 p3 +
   geom_histogram(binwidth = 1) +
   labs(title = "Day of Week Distribution", x = "Day of Week", y = "Count")
 
 
 # Weekend vs weekday
-prop.table(table(GymTraffic$is_weekend))
-p4 <- ggplot(GymTraffic, aes(factor(is_weekend, labels = c("Weekday", "Weekend"))))
+prop.table(table(gym_df$is_weekend))
+p4 <- ggplot(gym_df, aes(factor(is_weekend, labels = c("Weekday", "Weekend"))))
 p4 +
   geom_bar(aes(y = (..count..) / sum(..count..))) +
   scale_y_continuous(labels = scales::percent) + 
@@ -53,8 +53,8 @@ p4 +
 
 
 # Holidays
-prop.table(table(GymTraffic$is_holiday))
-p5 <- ggplot(GymTraffic, aes(factor(is_holiday, labels = c("Not Holiday", "Holiday"))))
+prop.table(table(gym_df$is_holiday))
+p5 <- ggplot(gym_df, aes(factor(is_holiday, labels = c("Not Holiday", "Holiday"))))
 p5 +
   geom_bar(aes(y = (..count..) / sum(..count..))) +
   scale_y_continuous(labels = scales::percent) + 
@@ -63,24 +63,24 @@ p5 +
 
 
 # Apparent temperature
-summary(GymTraffic$apparent_temperature)
-p2 <- ggplot(GymTraffic, aes(apparent_temperature))
+summary(gym_df$apparent_temperature)
+p2 <- ggplot(gym_df, aes(apparent_temperature))
 p2 +
   geom_histogram(binwidth = 1) +
   labs(title = "Apparent Temperature Distribution", x = "Apparent Temperature", y = "Count")
 
 
 # Temperature
-summary(GymTraffic$temperature)
-p2 <- ggplot(GymTraffic, aes(temperature))
+summary(gym_df$temperature)
+p2 <- ggplot(gym_df, aes(temperature))
 p2 +
   geom_histogram(binwidth = 1) +
   labs(title = "Temperature Distribution", x = "Temperature", y = "Count")
 
 
 # Start of semester
-prop.table(table(GymTraffic$is_start_of_semester))
-p5 <- ggplot(GymTraffic, aes(factor(is_start_of_semester, labels = c("Not Start", "Start"))))
+prop.table(table(gym_df$is_start_of_semester))
+p5 <- ggplot(gym_df, aes(factor(is_start_of_semester, labels = c("Not Start", "Start"))))
 p5 +
   geom_bar(aes(y = (..count..) / sum(..count..))) +
   scale_y_continuous(labels = scales::percent) + 
@@ -92,28 +92,42 @@ p5 +
 # Relationships -----------------------------------------------------------
 
 
-# People 
-p1 <- ggplot(GymTraffic, aes(factor(is_weekend, labels = c("Weekday", "Weekend")), number_people))
+# Gym attendance by day type
+p1 <- ggplot(gym_df, aes(factor(is_weekend, labels = c("Weekday", "Weekend")), number_people))
 p1 +
   stat_summary(fun.y = "mean", geom = "bar") +
-  labs(title = "Semester Start Distribution", x = "Day Type", y = "Mean People")
+  labs(title = "Weekday vs Weekend Gym Attendance", x = "Day Type", y = "Mean Number of People")
+
+
+# Relationship of temperature with gym traffic
+p2 <- ggplot(gym_df, aes(temperature, number_people))
+p2 +
+  geom_point() +
+  geom_smooth(method = "gam", formula = y ~ s(x, bs = "ps")) +
+  labs(title = "Gym Attendance by Temperature", x = "Temperature", y = "Number of People")
+
+
+
+#####################################################
+
+
 
 # Start of semester effect
-SemesterHist <- qplot(GymTraffic$number_people,
+SemesterHist <- qplot(gym_df$number_people,
                       geom = "histogram",
                       binwidth = 1,
                       main = "Distribution of People in Gym by Part of Semester",
                       xlab = "Number of People",
-                      color = factor(GymTraffic$is_start_of_semester))
+                      color = factor(gym_df$is_start_of_semester))
 SemesterHist + scale_colour_discrete(name = "Start of Semester", labels = c("No", "Yes"))
 
 # Temperature
-TempPlot <- ggplot(GymTraffic, aes(temperature, apparent_temperature))
+TempPlot <- ggplot(gym_df, aes(temperature, apparent_temperature))
 TempPlot + geom_point()
-TempPlot + geom_point(aes(color = GymTraffic$number_people))
+TempPlot + geom_point(aes(color = gym_df$number_people))
 
 # Relationship of temperature with gym traffic
-TempPeoplePlot <- ggplot(GymTraffic, aes(temperature, number_people))
+TempPeoplePlot <- ggplot(gym_df, aes(temperature, number_people))
 TempPeoplePlot +
   geom_point() +
   geom_smooth(method = "gam", formula = y ~ s(x, bs = "ps")) +
@@ -128,9 +142,9 @@ names(myColors) <- levels(dat$grp)
 colScale <- scale_colour_manual(name = "grp", values = myColors)
 
 # Relationship of timestamp with gym traffic
-TimePeoplePlot <- ggplot(GymTraffic, aes(timestamp, number_people, color = as.factor(is_weekend)))
-TimePeoplePlot <- ggplot(GymTraffic, aes(timestamp, number_people, color = as.factor(day_of_week)))
-TimePeoplePlot <- ggplot(GymTraffic, aes(timestamp, number_people, color = as.factor(is_holiday)))
+TimePeoplePlot <- ggplot(gym_df, aes(timestamp, number_people, color = as.factor(is_weekend)))
+TimePeoplePlot <- ggplot(gym_df, aes(timestamp, number_people, color = as.factor(day_of_week)))
+TimePeoplePlot <- ggplot(gym_df, aes(timestamp, number_people, color = as.factor(is_holiday)))
 TimePeoplePlot +
   geom_point() +
   geom_smooth(method = "gam", formula = y ~ s(x, bs = "ps")) +
