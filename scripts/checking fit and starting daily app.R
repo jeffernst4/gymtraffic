701d975f1d3c0a3f6a2c640799eb457074b12181
Jeff Ernst <jrernst@calpoly.edu>
2017-03-31 17:46:59 -0700

    starting the daily app

--- scripts/checking fit and starting daily app.R
+++ scripts/checking fit and starting daily app.R
@@ -8,7 +8,7 @@ for (i in 1:100) {
   lines(gym_df$timestamp[gym_df$date == unique(gym_df$date)[i]], gym_df$number_people[gym_df$date == unique(gym_df$date)[i]], type = "l", col = "black")
 }
 
-for (i in 1:100) {
+for (i in 1:11) {
   plot(test$timestamp[test$date == unique(test$date)[i]], rf_predictions[test$date == unique(test$date)[i]], type = "l", col = "red", main = paste("date:", unique(test$date)[i]))
   lines(test$timestamp[test$date == unique(test$date)[i]], test$number_people[test$date == unique(test$date)[i]], type = "l", col = "black")
 }
@@ -17,7 +17,20 @@ for (i in 1:100) {
 
 # Start Application for Daily Gym Traffic ---------------------------------
 
-df <- aggregate(predictions ~ hour, gym_df[gym_df$date == unique(test$date)[8], ], mean)
+gym_df <- cbind(gym_df, predictions)
+
+i = 20
+df <- aggregate(predictions ~ hour, gym_df[gym_df$date == unique(gym_df$date)[i], ], mean)
+df1 <- aggregate(predictions ~ hour, gym_df[(gym_df$date - 1) == unique(gym_df$date)[i], ], mean)
+df <- rbind(df[df$hour %in% 5:23, ], df1[df1$hour %in% 0:1, ])
+df$hour <- factor(df$hour, levels = c(5:23, 0:1))
 
 ggplot(df, aes(hour, predictions)) +
-  geom_bar(stat="identity")
+  geom_bar(stat = "identity") +
+  labs(title = format.Date(unique(gym_df$date)[i], "%B %d, %Y"),
+       x = "Hour",
+       y = "Number of People")
+
+
+
+
